d9670eda807c3b9aad4000eaa3aad31c2f56cf07
CCrisstian <crisstiann.c@gmail.com>
2024-01-28 10:37:48 -0300

    Agregando un valor Map<> a una variable del tipo Map<>

--- src/org/CCristian/Ejemplos/Map/Ejemplo_HashMap.java
+++ src/org/CCristian/Ejemplos/Map/Ejemplo_HashMap.java
@@ -1,14 +1,11 @@
 package org.CCristian.Ejemplos.Map;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Ejemplo_HashMap {
     public static void main(String[] args) {
 
-        Map<String, String> persona = new HashMap<>();
+        Map<String, Object> persona = new HashMap<>();
         /*Map<Key, Value>*/
         System.out.println("¿Contiene elementos? ==> " + !persona.isEmpty());
 
@@ -21,13 +18,27 @@ public class Ejemplo_HashMap {
         persona.put("apellidoPaterno","Doe");
         
         persona.put("email","john.doe@email.com");
-        persona.put("edad","30");
+        persona.put("edad",30);
+
+        /*---------------------------------------------------------*/
+        Map<String, String> direccion = new HashMap<>();
+        direccion.put("pais","USA");
+        direccion.put("estado","California");
+        direccion.put("ciudad","Santa Barbara");
+        direccion.put("calle","One Street");
+        direccion.put("numero","120");
+
+        persona.put("direccion",direccion);
+        /*Asignamos la variable direccion a persona*/
+        /*---------------------------------------------------------*/
+
+
         System.out.println(persona);
         
-        String nombre = persona.get("nombre");
+        String nombre = (String) persona.get("nombre");
         /*OBTENER un valor, persona.get(Key)*/
         System.out.println("nombre = " + nombre);
-        String apellido = persona.get("apellido");
+        String apellido = (String) persona.get("apellido");
         System.out.println("apellido = " + apellido);
 
         //String valor_apellidoPaterno = persona.remove("apellidoPaterno");
@@ -47,10 +58,10 @@ public class Ejemplo_HashMap {
         b2 = persona.containsValue("john.doe@email.com");
         System.out.println("¿Tiene el Value:john.doe@email.com? -> b2:" + b2);
 
-        Collection<String> valores = persona.values();
+        Collection<Object> valores = persona.values();
         /*Collection<Value>*/
         System.out.println("\n-------Obtener y Mostrar los VALORES (values)-------");
-        for (String v : valores){
+        for (Object v : valores){
             System.out.println("value: "+ v);
         }
 
@@ -63,20 +74,40 @@ public class Ejemplo_HashMap {
 
         System.out.println("\n-------ITERAR usando (entrySet)-------");
         /*Map.Entry<Key, Value>*/
-        for (Map.Entry<String, String> par: persona.entrySet()){
-            System.out.println("Key: "+par.getKey()+" => Value:" + par.getValue());
+        for (Map.Entry<String, Object> par: persona.entrySet()){
+            Object valor = par.getValue();
+            if (valor instanceof Map){
+                String nom = (String) persona.get("nombre");
+                System.out.println("Key: " + par.getKey());
+                Map<String, String> direccion_Map = (Map<String, String>) valor;
+                for (Map.Entry<String, String>parDir: direccion_Map.entrySet()){
+                    System.out.println("\t"+parDir.getKey() +" ==> "+parDir.getValue());
+                }
+
+            }else {
+                System.out.println("Key: " + par.getKey() + " => Value:" + valor);
+            }
         }
 
         System.out.println("\n-------ITERAR usando (getSet())-------");
         /*persona.get(Key)*/
         for (String llave : persona.keySet()){
-            String valor = persona.get(llave);
-            System.out.println("llave("+llave+") ==> valor: "+ valor);
+            Object valor = persona.get(llave);
+            if (valor instanceof Map){
+                String nom = (String) persona.get("nombre");
+                Map<String, String> direccion_Map = (Map<String, String>) valor;
+                System.out.println("El país de "+nom+" es: "+direccion_Map.get("pais"));
+                System.out.println("El estado de "+nom+" es: "+direccion_Map.get("estado"));
+                System.out.println("La ciudad de "+nom+" es: "+direccion_Map.get("ciudad"));
+
+            }else {
+                System.out.println("llave(" + llave + ") ==> valor: " + valor);
+            }
         }
 
         System.out.println("\n-------ITERAR usando FOREACH y Expresión LAMBDA-------");
         /*Map<llave, valor> persona*/ /*Expresión LAMBDA*/
-        persona.forEach((llave, valor) -> {
+        persona.forEach((llave, valor) ->{
             System.out.println("llave("+llave+") ==> valor: "+ valor);
         });
 
@@ -93,5 +124,17 @@ public class Ejemplo_HashMap {
         /*replace(Key, OldValue, NewValue) regresa un boolean*/
         System.out.println(persona);
 
+
+        Map<String, String> direccion_Persona = (Map<String, String>) persona.get("direccion");
+        String pais = direccion_Persona.get("pais");
+        String ciudad = direccion_Persona.get("ciudad");
+        String barrio = direccion_Persona.getOrDefault("barrio","La playa");
+        /*get(key) -> null en caso de que no exista la key que se busca*/
+        /*getOrDefault(key, defaultValue) -> defaultValue en caso de que no exista la key que se busca*/
+        System.out.println("El pais de " + nombre +" es: " + pais);
+        System.out.println("La ciudad de " + nombre +" es: " + ciudad);
+        System.out.println("El barrio de " + nombre +" es: " + barrio);
     }
+
 }
+
